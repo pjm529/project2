03a10f5843c264731075d307adcc4f27284c0b35
jmpark <pok_nima@naver.com>
2021-10-20 14:23:30 +0900

    파일 명 수정

--- project2/src/main/java/controller/menu/RecruitController.java
+++ project2/src/main/java/controller/menu/RecruitController.java
@@ -2,6 +2,8 @@ package controller.menu;
 
 import java.io.IOException;
 
+import java.util.List;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,11 +12,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import model.menu.RecruitVO;
 import service.menu.RecruitDAO;
 
-@WebServlet("/introduce/*")
-public class RecruitController extends HttpServlet {
-
+@WebServlet("/recruit/*")
+public class Recruitcontroller extends HttpServlet {
+	
 	private static final long serialVersionUID = 1L;
 	RecruitDAO recruitDAO;
 
@@ -34,34 +37,110 @@ public class RecruitController extends HttpServlet {
 		doHandle(request, response, session);
 	}
 
-	private void doHandle(HttpServletRequest request, HttpServletResponse response, HttpSession session)
-			throws ServletException, IOException {
-
+	private void doHandle(HttpServletRequest request, HttpServletResponse response, HttpSession session) throws ServletException, IOException {
+		
 		String nextPage = null;
 		request.setCharacterEncoding("utf-8");
 		response.setContentType("text/html;charset=utf-8");
-
+		
 		String action = request.getPathInfo(); // URL에서 요청명을 가져옴.
+		
+		if(action == null || action.equals("/listRecruit")) { // 최초 요청이거나 listMembers.do일때
+			
+			 String search_select = request.getParameter("search_select");
+			 String search_text = request.getParameter("search_text");
+			
+			List<RecruitVO> recruitList = recruitDAO.listRecruits(search_select, search_text);
+			
+			request.setAttribute("recruitList", recruitList);
+			nextPage = "/homepage/menu/recruit/recruit.jsp";
+			
+		} else if(action.equals("/addRecruit")) {
+				
+			String title = request.getParameter("title");
+			String content = request.getParameter("content");
+			String writer = request.getParameter("writer");
+			String writer_id = (String)session.getAttribute("id");
+			String training_period = request.getParameter("training_period");
+			String recruit_period = request.getParameter("recruit_period");
+			String time = request.getParameter("time");
+			String count = request.getParameter("count");
+			String location = request.getParameter("location");
+			String professor = request.getParameter("professor");
+			
+			RecruitVO recruitVO = new RecruitVO(title, content, writer, writer_id, training_period, recruit_period
+					, time, count, location, professor);
+			
+			recruitDAO.addRecruit(recruitVO);
+			
+			List<RecruitVO> recruitList = recruitDAO.listRecruits(null, null);
+			request.setAttribute("recruitList", recruitList);
+			nextPage = "/homepage/menu/recruit/recruit.jsp";
+			
+		} else if(action.equals("/recruitForm")) {
+			
+			nextPage = "/homepage/menu/recruit/insertForm.jsp";
+			
+		} else if(action.equals("/viewRecruit")) {
+			
+			String num = request.getParameter("num");
+			RecruitVO recruitInfo = recruitDAO.findRecruit(num);
+			
+			request.setAttribute("recruitInfo", recruitInfo);
+			
+			nextPage = "/homepage/menu/recruit/viewRecruit.jsp";
+			
+		} else if(action.equals("/delRecruit")) {
+			
+			String num = request.getParameter("num");
+			String sessId = (String)session.getAttribute("id");
+			String writer_id = request.getParameter("writer_id");
+			
+			recruitDAO.delRecruit(num, sessId, writer_id);
+			
+			List<RecruitVO> recruitList = recruitDAO.listRecruits(null, null);
+			request.setAttribute("recruitList", recruitList);
+			nextPage = "/homepage/menu/recruit/recruit.jsp";
 
-		if (action == null || action.equals("/introduce")) { // 최초 요청이거나 listMembers.do일때
-
-			nextPage = "/homepage/menu/introduce/introduce.jsp";
-
-		} else if (action.equals("/introduce2")) {
-
-			nextPage = "/homepage/menu/introduce/introduce2.jsp";
-
-		} else if (action.equals("/introduce3")) {
-
-			nextPage = "/homepage/menu/introduce/introduce3.jsp";
-
+		} else if(action.equals("/modRecruitForm")) {
+			
+			String num = request.getParameter("num");
+			RecruitVO recruitInfo = recruitDAO.findRecruit(num);
+			
+			request.setAttribute("recruitInfo", recruitInfo);
+			request.setAttribute("writer_id", recruitInfo.getWriter_id());
+			
+			nextPage = "/homepage/menu/recruit/updateForm.jsp";
+			
+		} else if(action.equals("/modRecruit")) {
+			
+			String num = request.getParameter("num");
+			String title = request.getParameter("title");
+			String content = request.getParameter("content");
+			String writer = request.getParameter("writer");
+			String writer_id = (String)session.getAttribute("id");
+			String training_period = request.getParameter("training_period");
+			String recruit_period = request.getParameter("recruit_period");
+			String time = request.getParameter("time");
+			String count = request.getParameter("count");
+			String location = request.getParameter("location");
+			String professor = request.getParameter("professor");
+			
+			RecruitVO recruitVO = new RecruitVO(num, title, content, writer, writer_id, training_period, recruit_period
+					, time, count, location, professor, null);
+			
+			recruitDAO.modRecruit(recruitVO);
+			
+			RecruitVO recruitInfo = recruitDAO.findRecruit(num);
+			request.setAttribute("recruitInfo", recruitInfo);
+			nextPage = "/homepage/menu/recruit/viewRecruit.jsp";
+	
 		} else {
-
+				
 			nextPage = "/error/error404.jsp";
-
 		}
-
-		RequestDispatcher dispatch = request.getRequestDispatcher(nextPage);
+		
+		RequestDispatcher dispatch = request.getRequestDispatcher(nextPage); 
 		dispatch.forward(request, response);
 	}
 
